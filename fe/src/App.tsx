5a129f4a58003878f4793cd121f43ed162512cdf
Matheus <mtguerson@gmail.com>
2024-03-20 15:25:42 -0400

    setting up global styles and creating Header component

---
+++ fe/src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
+import { Header } from './components/Header/index';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
+}
