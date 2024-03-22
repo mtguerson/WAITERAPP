4b97ff7f268bd71e9147b91b3a56dba7f987073b
Matheus Guerson <mtguerson@gmail.com>
2024-03-22 13:20:00 -0400

    configuring fonts

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,18 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-500': require('./src/assets/fonts/GeneralSans-SemiBold.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
