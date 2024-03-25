cc71b289fa52804814e8f012fc0a2dc9298c20c8
Matheus <mtguerson@gmail.com>
2024-03-24 21:01:03 -0400

    wip: Categories

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -1,3 +1,5 @@
+import { FlatList } from 'react-native';
+
 import { Category, Icon } from './styles';
 import { Text } from '../Text';
 
@@ -5,16 +7,21 @@ import { categories } from '../../mocks/categories';
 
 export function Categories() {
   return (
-    categories.map((category) => {
-      return (
-        <Category key={category._id}>
+    <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      data={categories}
+      contentContainerStyle={{ paddingRight: 24 }}
+      keyExtractor={category => category._id}
+      renderItem={({ item: category }) => (
+        <Category>
           <Icon>
             <Text>{category.icon}</Text>
           </Icon>
 
-          <Text>{category.name}</Text>
+          <Text size={14} weight="600">{category.name}</Text>
         </Category>
-      );
-    })
+      )}
+    />
   );
 }
