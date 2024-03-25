e3ef60d2fb19c927a2ecfe5f17487e3fbc993b64
Matheus <mtguerson@gmail.com>
2024-03-25 13:06:22 -0400

    adding pressable styles on categories

--- app/src/components/Categories/index.tsx
+++ app/src/components/Categories/index.tsx
@@ -4,8 +4,16 @@ import { Category, Icon } from './styles';
 import { Text } from '../Text';
 
 import { categories } from '../../mocks/categories';
+import { useState } from 'react';
 
 export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
   return (
     <FlatList
       horizontal
@@ -13,15 +21,21 @@ export function Categories() {
       data={categories}
       contentContainerStyle={{ paddingRight: 24 }}
       keyExtractor={category => category._id}
-      renderItem={({ item: category }) => (
-        <Category>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
 
-          <Text size={14} weight="600">{category.name}</Text>
-        </Category>
-      )}
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+            </Icon>
+
+            <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
+              {category.name}
+            </Text>
+          </Category>
+        );
+      }}
     />
   );
 }
