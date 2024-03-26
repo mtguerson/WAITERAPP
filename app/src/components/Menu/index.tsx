61e0aae5d2b8e79e23eb133a3ff8a30680f97c65
Matheus <mtguerson@gmail.com>
2024-03-26 08:43:45 -0400

    wip: ProductModal

--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -6,36 +6,47 @@ import { Text } from '../Text';
 import { Product, ProductImage, ProductDetails, Separator, AddToCartButton } from './styles';
 import { formatCurrency } from '../../utils/formatCurrency';
 import { PlusCircle } from '../Icons/PlusCircle';
+import { ProductModal } from '../ProductModal';
+import { useState } from 'react';
 
 export function Menu() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   return (
-    <FlatList
-      data={products}
-      style={{ marginTop: 32 }}
-      contentContainerStyle={{ paddingHorizontal: 24 }}
-      keyExtractor={product => product._id}
-      ItemSeparatorComponent={Separator}
-      renderItem={({ item: product }) => (
-        <Product>
-          <ProductImage
-            source={{
-              uri: `http://192.168.100.172:3001/uploads/${product.imagePath}`,
-            }}
-          />
+    <>
+      <ProductModal
+        visible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+      />
 
-          <ProductDetails>
-            <Text weight="600">{product.name}</Text>
-            <Text size={14} color="#666" stlye={{ marginVertical: 8 }}>
-              {product.description}
-            </Text>
-            <Text size={14} weight="600">{formatCurrency(product.price)}</Text>
-          </ProductDetails>
+      <FlatList
+        data={products}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        keyExtractor={product => product._id}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item: product }) => (
+          <Product onPress={() => setIsModalVisible(true)}>
+            <ProductImage
+              source={{
+                uri: `http://192.168.100.172:3001/uploads/${product.imagePath}`,
+              }}
+            />
 
-          <AddToCartButton>
-            <PlusCircle />
-          </AddToCartButton>
-        </Product>
-      )}
-    />
+            <ProductDetails>
+              <Text weight="600">{product.name}</Text>
+              <Text size={14} color="#666" stlye={{ marginVertical: 8 }}>
+                {product.description}
+              </Text>
+              <Text size={14} weight="600">{formatCurrency(product.price)}</Text>
+            </ProductDetails>
+
+            <AddToCartButton>
+              <PlusCircle />
+            </AddToCartButton>
+          </Product>
+        )}
+      />
+    </>
   );
 }
--- app/src/components/ProductModal.tsx
+++ app/src/components/ProductModal.tsx
@@ -1,3 +1,20 @@
-export function ProductModal() {
+import { Modal } from 'react-native';
+import { Text } from './Text';
 
+interface ProductModalProps {
+  visible: boolean;
+  onClose: () => void;
+}
+
+export function ProductModal({ visible, onClose }: ProductModalProps) {
+  return (
+    <Modal
+      visible={visible}
+      animationType="slide"
+      presentationStyle="pageSheet"
+      onRequestClose={onClose}
+    >
+      <Text>ProductModal</Text>
+    </Modal>
+  );
 }
