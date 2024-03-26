e91376cf9b0ea1cad76af9d3816789565e6168b4
Matheus Guerson <mtguerson@gmail.com>
2024-03-25 21:29:41 -0400

    finalizing Overlay and Modal

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -4,8 +4,17 @@ import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
 import { Button } from '../components/Button';
+import { TableModal } from '../components/TableModal';
+import { useState } from 'react';
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -22,11 +31,19 @@ export function Main() {
       </Container>
       <Footer>
         <FooterContainer>
-          <Button onPress={() => alert('Pressionou')}>
-            Novo Pedido
-          </Button>
+          {!selectedTable &&
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          }
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
